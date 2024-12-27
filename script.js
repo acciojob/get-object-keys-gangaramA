83a186841693a5ce68fe3920c56ed38e4a982cae
gangaramA <150507677+gangaramA@users.noreply.github.com>
2024-12-27 19:03:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const student = {
+  name: 'John Doe'
+};
+
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+};
+
+console.log(student.getKeys()); // Output: ["name"]
+
